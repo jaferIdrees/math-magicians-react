eba08ad96aab82993bc10129c82b1a87772de528
jaferIdrees <jaferll@gmail.com>
2022-08-03 01:00:11 +0300

    Make changes on alculator UI -

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (<Calculator />);
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (<Calculator />);
+  }
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function CalculatorKey(props) {
+/* function CalculatorKey(props) {
   const props1 = { ...props };
-  return (<input type="button" className={(Number(props1.value) > 0) ? 'number' : 'key'} value={props1.value} />);
+  return (<input type="button"
+  className={(Number(props1.value) > 0) ? 'number' : 'key'} value={props1.value} />);
 }
-
+ */
 const resultElement = <input type="number" className="result" value="0" disabled />;
 
 class Calculator extends React.Component {
@@ -14,7 +15,13 @@ class Calculator extends React.Component {
     1, 2, 3, '+',
     0, '.', '='];
 
-  keysElements = this.keys.map((key) => (<CalculatorKey value={key} key={key.toString()} />));
+  keysElements = this.keys.map((key) => (<this.CalculatorKey value={key} key={key.toString()} />));
+
+  // eslint-disable-next-line class-methods-use-this
+  CalculatorKey(props) {
+    const props1 = { ...props };
+    return (<input type="button" className={(Number(props1.value) > 0) ? 'number' : 'key'} value={props1.value} />);
+  }
 
   render() {
     return (
