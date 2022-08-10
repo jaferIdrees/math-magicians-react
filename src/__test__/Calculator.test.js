fd0e9b0a8be5860ceca788cdb8ca6dec608dd846
Raminka <raminka13@hotmail.com>
2022-08-10 16:55:20 -0500

    Add operations testing to Calculator RTL

--- src/__test__/Calculator.test.js
+++ src/__test__/Calculator.test.js
@@ -18,7 +18,33 @@ describe('Calculator component testing using React testing library', () => {
     fireEvent.click(screen.getByTestId('+'));
     fireEvent.click(screen.getByTestId('1'));
     fireEvent.click(screen.getByTestId('='));
-    screen.debug();
     expect(screen.getByTestId('resultText').value).toBe('2');
+  });
+
+  it('Test user interaction press 4 minus 1 to be 3', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByTestId('4'));
+    fireEvent.click(screen.getByTestId('-'));
+    fireEvent.click(screen.getByTestId('1'));
+    fireEvent.click(screen.getByTestId('='));
+    expect(screen.getByTestId('resultText').value).toBe('3');
+  });
+
+  it('Test user interaction press 3 times 3 to be 9', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByTestId('3'));
+    fireEvent.click(screen.getByTestId('x'));
+    fireEvent.click(screen.getByTestId('3'));
+    fireEvent.click(screen.getByTestId('='));
+    expect(screen.getByTestId('resultText').value).toBe('9');
+  });
+
+  it('Test user interaction press 9 divide 3 to be 3', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByTestId('9'));
+    fireEvent.click(screen.getByTestId('÷'));
+    fireEvent.click(screen.getByTestId('3'));
+    fireEvent.click(screen.getByTestId('='));
+    expect(screen.getByTestId('resultText').value).toBe('3');
   });
 });
