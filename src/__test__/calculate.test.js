26b65ea66bf9c3c8a48d9dc98814b6f31b3a7bb1
jaferIdrees <jaferll@gmail.com>
2022-08-10 23:03:58 +0300

    Add test for calculate-addition function

---
+++ src/__test__/calculate.test.js
@@ -0,0 +1,15 @@
+import calculate from "../logic/calculate";
+
+describe('Test plus operation', () => {
+  it('Assert the sum of one and two is equal to three', () => {
+    expect(() => {
+      calculate({
+        total: 0,
+        next: 2,
+        operation: '+',
+      }, 1)
+        .total.toBe(3);
+    });
+  });
+});
+
