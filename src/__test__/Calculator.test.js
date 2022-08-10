a53e211152d3baa3f19a24951f059fae1b810b94
jaferIdrees <jaferll@gmail.com>
2022-08-11 00:49:07 +0300

    Add testing using React testing library

--- src/__test__/Calculator.test.js
+++ src/__test__/Calculator.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
 describe('Calculator component testing using snapshots', () => {
@@ -7,5 +8,17 @@ describe('Calculator component testing using snapshots', () => {
     const tree = renderer
       .create(<Calculator />);
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Calculator component testing using React testing library', () => {
+  it('Test user interaction press 1 plus 1 to be 2', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByTestId('1'));
+    fireEvent.click(screen.getByTestId('+'));
+    fireEvent.click(screen.getByTestId('1'));
+    fireEvent.click(screen.getByTestId('='));
+    screen.debug();
+    expect(screen.getByTestId('resultText').value).toBe('2');
   });
 });
